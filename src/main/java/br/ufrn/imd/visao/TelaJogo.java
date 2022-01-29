9641748bf4651678d3a73192b0e999587dc6c1aa
Lucas Nascimento <lucasnascimentoribeiro13@gmail.com>
2022-01-29 13:40:57 -0300

    Adicionar xeque mate e alterar mensagem

--- src/main/java/br/ufrn/imd/visao/TelaJogo.java
+++ src/main/java/br/ufrn/imd/visao/TelaJogo.java
@@ -26,8 +26,9 @@ public class TelaJogo extends JFrame implements ActionListener {
 
 	public TelaJogo() {
 		iniciarJogo();
-		
+
 		setSize(800,600);
+		setLocationRelativeTo(null);
 		setResizable(false);
 		setTitle("Jogo de Xadrez");
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -36,20 +37,8 @@ public class TelaJogo extends JFrame implements ActionListener {
 	public void iniciarJogo() {
 		this.jogo = new Jogo();
 
-		Peca[][] campo = this.jogo.getTabuleiro().getCampo();
-		for (int i = 0; i < 8; i++) {
-			for (int j = 0; j < 8; j++) {
-				Peca peca = campo[i][j];
-				if (peca != null && peca.getCor() == CorDaPeca.PRETA && !(peca instanceof Rei)) {
-					campo[i][j] = null;
-				}
-				if (peca != null && peca instanceof Peao) {
-					campo[i][j] = null;
-				}
-			}
-		}
-
 		Container ctn = getContentPane();
+		ctn.removeAll();
 		ctn.setLayout(new GridLayout(8, 9, 0, 0));
 
 		for(int a = 0; a < 8; a++) {
@@ -136,6 +125,7 @@ public class TelaJogo extends JFrame implements ActionListener {
 
 				if (reiEmXeque != null) {
 					this.xeque = reiEmXeque.getPosicao();
+					this.verificarXequeMate();
 				} else {
 					this.xeque = null;
 				}
@@ -177,6 +167,31 @@ public class TelaJogo extends JFrame implements ActionListener {
 		}
 	}
 
+	private void verificarXequeMate() {
+		if (this.xeque == null) {
+			return;
+		}
+
+		Tabuleiro tabuleiro = this.jogo.getTabuleiro();
+		Peca peca = tabuleiro.getCampo()[xeque.getLinha()][xeque.getColuna()];
+		List<Posicao> posicoes = peca.informarPossiveisJogadas(tabuleiro);
+
+		// Remover possiveis jogadas que não tiram o xeque do rei
+		for (int i = 0; i < posicoes.size(); i++) {
+			Posicao posicao = posicoes.get(i);
+			boolean movimentoValido = this.jogo.isMovimentoValidoTendoReiEmXeque(peca, posicao);
+			if (!movimentoValido)
+				posicoes.remove(i--);
+		}
+
+		boolean xequeMate = posicoes.size() == 0;
+		if (xequeMate) {
+			this.jogo.setEstado(EstadoDeJogo.GAMEOVER);
+			this.xeque = null;
+			atualizarTabuleiro();
+		}
+	}
+
 	private void informarFimDeJogo() {
 		String vencedor = "Vitória das peças ";
 
@@ -186,23 +201,12 @@ public class TelaJogo extends JFrame implements ActionListener {
 			vencedor += "brancas.";
 		}
 
-		JPanel mensagem = new JPanel();
-		mensagem.setSize(800,600);
-		mensagem.setVisible(true);
-
-		JLabel vcd = new JLabel(vencedor);
-		vcd.setBounds(100,100,100,100);
-
-		JButton denovo = new JButton("Recomeçar");
-		denovo.setBounds(200,200,100,100);
-		denovo.addActionListener(this);
-		denovo.setActionCommand("reset");
-
-		Container ctn = getContentPane();
-		ctn.removeAll();
-		ctn.add(mensagem);
-		ctn.add(vcd);
-		ctn.add(denovo);
+		String[] opcoes = {"Recomeçar"};
+		int recomecar = JOptionPane.showOptionDialog(null, vencedor, "Fim de jogo",
+				JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, opcoes, opcoes[0]);
+		if (recomecar == 0) {
+			this.iniciarJogo();
+		}
 	}
 
 }
